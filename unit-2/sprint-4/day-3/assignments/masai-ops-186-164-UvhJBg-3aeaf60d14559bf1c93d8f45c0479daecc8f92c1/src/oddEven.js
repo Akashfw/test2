8873b9be0fafad95bb852275926d255d5db9b097
unknown <shuklaakash30278@gmail.com>
2022-11-04 07:44:36 -0700

    completed

---
+++ unit-2/sprint-4/day-3/assignments/masai-ops-186-164-UvhJBg-3aeaf60d14559bf1c93d8f45c0479daecc8f92c1/src/oddEven.js
@@ -0,0 +1,31 @@
+
+function generateNumber() {
+  // generate a random integer(hint : Math.random)
+ let num=Math.floor( Math.random()*100);
+ document.querySelector("#number").innerText=num;
+ checkOddEven(num);
+}
+
+function checkOddEven(num) {
+  // logic for even / odd
+  let param=num;
+  if(param%2==0){
+    document.querySelector("#odd-even").innerText="The number is even"
+  }else{
+    document.querySelector("#odd-even").innerText="The number is odd"
+  }
+}
+
+window.onload = function () {
+  // add event listeners to the button here
+  document.querySelector("#generate-number").addEventListener("click",generateNumber);
+};
+
+// donot change the following export statement
+
+if (typeof exports !== "undefined") {
+  module.exports = {
+    generateNumber,
+    checkOddEven,
+  };
+}
