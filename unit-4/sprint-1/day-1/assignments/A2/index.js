ad70c7a9286119352adebaa0626c5a61a807e649
Akashfw <shuklaakash30278@gmail.com>
2022-12-25 17:57:43 +0530

    copm A2

---
+++ unit-4/sprint-1/day-1/assignments/A2/index.js
@@ -0,0 +1,55 @@
+const fs= require("fs");
+
+const val= process.argv.slice(2)
+
+let x= val[0];
+
+if(x=="read"){
+    fs.readFile(`./${val[1]}`,{encoding:"utf-8"},(err,data)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log(data)
+        }
+    })
+}
+
+if(x=="append"){
+    fs.appendFile(`./${val[2]}`,`\n${val[1]}`,(err)=>{
+        if(err){
+            console.log("cannot append")
+        }else{
+            console.log("appended data")
+        }
+    })
+};
+
+if(x=="delete"){
+    fs.rm(`./${val[1]}`,(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("file deleted")
+        }
+    })
+};
+
+if(x=="create"){
+    fs.writeFile(`./${val[1]}`,"this is my file",(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("file has created")
+        }
+    })
+}
+
+if(x=="rename"){
+    fs.rename(`./${val[1]}`,`./${val[2]}`,(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("name changed")
+        }
+    })
+}
